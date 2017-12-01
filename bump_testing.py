942e4bc9152b092bf9c181523d78011bb92e5467
Pat Graham <pdgra1@student.monash.edu>
2017-12-01 12:23:57 +1100

    Delete i2c.py
    Former-commit-id: 395b5bb955e48d4aae6e5c8e12672268e1c12b45

--- bump_testing.py
+++ bump_testing.py
@@ -48,6 +48,107 @@ G_GAIN = 0.070
 LP = 0.041      # Loop period = 41ms.   This needs to match the time it takes each loop to run
 AA = 0.80      # Complementary filter constant
 
+# COMMANDS TO WRITE ON I2C BUS
+
+
+def writeACC(register, value):
+    bus.write_byte_data(ACC_ADDRESS, register, value)
+    return -1
+
+
+def writeMAG(register, value):
+    bus.write_byte_data(MAG_ADDRESS, register, value)
+    return -1
+
+
+def writeGRY(register, value):
+    bus.write_byte_data(GYR_ADDRESS, register, value)
+    return -1
+
+# COMMANDS TO READ FROM I2C BUS
+
+
+def readACCx():
+    if MAG_ADDRESS == 0x1E:
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_A)
+    else:
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_X_L_XL)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_X_H_XL)
+    acc_combined = (acc_l | acc_h << 8)
+    return acc_combined if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCy():
+    if MAG_ADDRESS == 0x1E:
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_A)
+    else:
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_XL)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Y_H_XL)
+    acc_combined = (acc_l | acc_h << 8)
+    return acc_combined if acc_combined < 32768 else acc_combined - 65536
+
+
+def readACCz():
+    if MAG_ADDRESS == 0x1E:
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_A)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_A)
+    else:
+        acc_l = bus.read_byte_data(ACC_ADDRESS, OUT_Z_L_XL)
+        acc_h = bus.read_byte_data(ACC_ADDRESS, OUT_Z_H_XL)
+    acc_combined = (acc_l | acc_h << 8)
+    return acc_combined if acc_combined < 32768 else acc_combined - 65536
+
+
+def readMAGx():
+    mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_X_L_M)
+    mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_X_H_M)
+    mag_combined = (mag_l | mag_h << 8)
+
+    return mag_combined if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGy():
+    mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Y_L_M)
+    mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Y_H_M)
+    mag_combined = (mag_l | mag_h << 8)
+
+    return mag_combined if mag_combined < 32768 else mag_combined - 65536
+
+
+def readMAGz():
+    mag_l = bus.read_byte_data(MAG_ADDRESS, OUT_Z_L_M)
+    mag_h = bus.read_byte_data(MAG_ADDRESS, OUT_Z_H_M)
+    mag_combined = (mag_l | mag_h << 8)
+
+    return mag_combined if mag_combined < 32768 else mag_combined - 65536
+
+
+def readGYRx():
+    gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_X_L_G)
+    gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_X_H_G)
+    gyr_combined = (gyr_l | gyr_h << 8)
+
+    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
+
+
+def readGYRy():
+    gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Y_L_G)
+    gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Y_H_G)
+    gyr_combined = (gyr_l | gyr_h << 8)
+
+    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
+
+
+def readGYRz():
+    gyr_l = bus.read_byte_data(GYR_ADDRESS, OUT_Z_L_G)
+    gyr_h = bus.read_byte_data(GYR_ADDRESS, OUT_Z_H_G)
+    gyr_combined = (gyr_l | gyr_h << 8)
+
+    return gyr_combined if gyr_combined < 32768 else gyr_combined - 65536
+
+
 # initialise the accelerometer
 if MAG_ADDRESS == 0x1E:
     writeACC(CTRL_REG1_XM, 0b01100111)  # z,y,x axis enabled, continuous update,  100Hz data rate
