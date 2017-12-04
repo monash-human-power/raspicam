395c7d401ccd383548691fd2aea6138c41b8ff54
Pat Graham <pdgra1@student.monash.edu>
2017-12-04 12:02:25 +1100

    Update code to reflect ozzmaker strategy
    Former-commit-id: 816243f2cba0d25bc2c070fa96a8f4b28a41ded4

--- IMU/BumpTest.py
+++ IMU/BumpTest.py
@@ -8,43 +8,16 @@
 # ---------------------------------------------------------------------
 
 # ---------------------- Import required libraries --------------------
-
-from i2c import MAG_ADDRESS,ACC_ADDRESS,GYR_ADDRESS,writeACC,writeMAG,writeGRY,readACCx,readACCy,readACCz,readMAGx,readMAGy,readMAGz,readGYRx,readGYRy,readGYRz
-from time import localtime, strftime
 import time
-
-if MAG_ADDRESS == 0x1E:
-    from LSM9DS0 import *
-else:
-    from LSM9DS1 import *
+import math
+import IMU
+import datetime
+import os
 
 # ---------------------- Initialise berryIMU --------------------------
 
-# initialise the accelerometer
-if MAG_ADDRESS == 0x1E:
-    writeACC(CTRL_REG1_XM, 0b10100111)  # z,y,x axis enabled, continuous update,  1600 Hz data rate
-    writeACC(CTRL_REG2_XM, 0b00100000)  # +/- 16G full scale, 773 Hz Anti-Aliasing Bandwidth
-else:
-    # 119 Hz data rate, +/- 16G full scale, 408 Hz Anti-Aliasing Bandwidth
-    writeACC(CTRL_REG6_XL, 0b01101000)
-    writeACC(CTRL_REG2_XM, 0b00100000)  # +/- 16G full scale
-
-    # initialise the magnetometer
-if MAG_ADDRESS == 0x1E:
-    writeMAG(CTRL_REG5_XM, 0b11110100)  # Temp enable, M data rate = 100Hz
-    writeMAG(CTRL_REG6_XM, 0b01100000)  # +/-12gauss
-    writeMAG(CTRL_REG7_XM, 0b00000000)  # Continuous-conversion mode
-else:
-    writeMAG(CTRL_REG1_XM, 0b11110000)  # Temp enable, High Resolution, M data rate = 50Hz
-    writeMAG(CTRL_REG2_XM, 0b01100000)  # +/-12gauss
-    writeMAG(CTRL_REG3_XM, 0b00000000)  # Continuous-conversion mode
-
-# initialise the gyroscope
-if MAG_ADDRESS == 0x1E:
-    writeGRY(CTRL_REG1_G, 0b11001111)  # ODR = 760 Hz, Cutoff = 12.5 Hz
-    writeGRY(CTRL_REG4_G, 0b00110000)  # Continuous update, 2000 dps full scale
-else:
-    writeGRY(CTRL_REG1_G, 0b01111000)  # ODR = 119 Hz, Cutoff = 14 Hz, 2000 dps
+IMU.detectIMU()  # Detect if BerryIMUv1 or BerryIMUv2 is connected.
+IMU.initIMU()  # Initialise the accelerometer, gyroscope and compass
 
 # ---------------------- Start Program --------------------------------
 
@@ -60,27 +33,27 @@ while True:
     try:
         # Record time when i2c read starts
         start_read = time.time()
-        
+
         # Read our accelerometer,gyroscope and magnetometer  values
-        ACCx = readACCx()
-        ACCy = readACCy()
-        ACCz = readACCz()
+        ACCx = IMU.readACCx()
+        ACCy = IMU.readACCy()
+        ACCz = IMU.readACCz()
 
         # Record time when i2c read ends, calculate difference
         end_read = time.time()
-	time_elapsed = end_read - init_time
+        time_elapsed = end_read - init_time
 
-	# Write information to file
-        data = "\n%5.20f,%5.2f,%5.2f,%5.2f" % (time_elapsed,ACCx,ACCy,ACCz)
+        # Write information to file
+        data = "\n%5.20f,%5.2f,%5.2f,%5.2f" % (time_elapsed, ACCx, ACCy, ACCz)
         file.write(data)
 
         # Record time when file write ends
         end_write = time.time()
 
         # Output loop frequency
-        write_freq = 1/(end_write-start_read)
+        write_freq = 1 / (end_write - start_read)
         print "%5.5f" % write_freq
-        
+
     except KeyboardInterrupt:
 
         # End program
