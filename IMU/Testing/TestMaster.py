52eff2e792d44ee6bf3d88f24aa3d271f0f64fce
Pat <pdgra1@student.monash.edu>
2017-12-10 13:35:11 +1100

    Improved TestMaster and Graphing code

--- IMU/Testing/TestMaster.py
+++ IMU/Testing/TestMaster.py
@@ -19,20 +19,7 @@ while True:
         print("Executing...")
         sleep(1)
     except KeyboardInterrupt:
-        # Get process id
-        pid1 = p1.pid
-        pid2 = p2.pid
-
-        # Try to terminate gracefully
         p1.terminate()
         p2.terminate()
-
-        # Check if you need to force kill
-        try:
-            os.kill(pid1, 0)
-            os.kill(pid2, 0)
-            p1.kill()
-            p2.kill()
-            print ("\nForced kill\n")
-        except OSError, e:
-            print("\nTerminated Gracefully\n")
+	print("\nExecution Terminated\n")
+	break
