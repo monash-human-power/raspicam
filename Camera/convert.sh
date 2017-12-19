8413d8e8a311ba9e6323886c7ab09f729890c237
pdgra1 <pdgra1@student.monash.edu>
2017-12-20 09:55:29 +1100

    Fixed Camera script for primary
    Former-commit-id: a937a6ed69cc8a2d918dc6dd8d10af63bb7a0b65

--- Camera/convert.sh
+++ Camera/convert.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.mkv
-ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /media/pi/7A49-A5AB/Recording_$1.mkv
+
+ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -vcodec copy /media/pi/7A49-A5AB/Recording_$1.mkv
 
 sudo rm /home/pi/Documents/MHP_raspicam/Camera/Video/*.h264
