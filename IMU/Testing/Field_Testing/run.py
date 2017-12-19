9c8e44ed0241d394d793d17895475496c4312fe2
pdgra1 <pdgra1@student.monash.edu>
2017-12-19 09:34:52 +0000

    Added Field_Testing Section
    Former-commit-id: b51768edd16fd6bbddf6e921c7e097cd3d181cc7

---
+++ IMU/Testing/Field_Testing/run.py
@@ -0,0 +1,27 @@
+# ---------------------------------------------------------------------
+# Last Modified:
+#   9-12-2017
+# Description:
+#   This program is the test master. It decides which modules run append
+#   and in what order.
+# ---------------------------------------------------------------------
+
+import subprocess
+from time import sleep
+
+#p1 = subprocess.Popen(["python", "datav1.py"])
+#print("\nStarting Data Recording v1...")
+#sleep(1)
+
+p2 = subprocess.Popen(["python", "datav2.py"])
+print("\nStarting Data Recording v2...")
+sleep(1)
+
+while True:
+    try:
+        sleep(1)
+    except KeyboardInterrupt:
+        #p1.terminate()
+        p2.terminate()
+	print("\n\nExecution Terminated.\n")
+	break
