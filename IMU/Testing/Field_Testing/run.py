58bfe3a4d10aa76529e924bfd43085315448d077
Pat Graham <pdgra1@student.monash.edu>
2017-12-20 06:28:58 +1100

    Added back in graphing capability
    Former-commit-id: 5049202fccd090f74d8296205a0641329683d3eb

--- IMU/Testing/Field_Testing/run.py
+++ IMU/Testing/Field_Testing/run.py
@@ -11,7 +11,7 @@ from time import sleep
 
 #p1 = subprocess.Popen(["python", "datav1.py"])
 #print("\nStarting Data Recording v1...")
-#sleep(1)
+# sleep(1)
 
 p2 = subprocess.Popen(["python", "datav2.py"])
 print("\nStarting Data Recording v2...")
@@ -21,7 +21,11 @@ while True:
     try:
         sleep(1)
     except KeyboardInterrupt:
-        #p1.terminate()
+        # p1.terminate()
         p2.terminate()
-	print("\n\nExecution Terminated.\n")
-	break
+        print("\n\nExecution Terminated.\n")
+        break
+
+p3 = subprocess.Popen(["python", "graph.py"])
+print("\nPlotting Graphs!\n\n")
+sleep(1)
