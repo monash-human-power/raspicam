2c04ee013d4009336e92253dfa4d8e1608636913
pdgra1 <pdgra1@student.monash.edu>
2017-12-20 09:30:50 +1100

    Changed code to suit primary

--- Camera/convert.sh
+++ Camera/convert.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.mkv
+#ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.mkv
+ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /media/pi/7A49-A5AB/Recording_$1.mkv
+
+sudo rm /home/pi/Documents/MHP_raspicam/Camera/Video/*.h264
