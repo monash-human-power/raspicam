9bbe16d579fa9b5b27766945f0bb6538090a628e
Pat <pdgra1@student.monash.edu>
2017-12-31 13:55:37 +1100

    Fixed directory issue

--- IMU/Testing/sendcsv.sh
+++ IMU/Testing/sendcsv.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 printf "Sending data csv file....\n\n"
-scp /home/pi/Documents/MHP_raspicam/IMU/Testing/Test_Data/Pi_Back.csv pi@patpi.local:/home/pi/Documents/Received_Data/Pi_Zero_2.csv
+time=$(date +%H-%M-%S)
+scp /home/pi/Documents/MHP_raspicam/IMU/Testing/Test_Data/Pi_Back.csv pi@patpi.local:/home/pi/Documents/MHP_raspicam/IMU/Testing/Received_Data/pz2_$time.csv
 printf "\nData sent.\n\n"
 
 exit 0
