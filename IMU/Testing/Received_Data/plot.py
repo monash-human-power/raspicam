25ee84b9b33ca22a1dc8b9ff01b9857e4a9a06d4
Pat <pdgra1@student.monash.edu>
2018-01-05 14:51:55 +1100

    Added Data, played with sensitivity

--- IMU/Testing/Received_Data/plot.py
+++ IMU/Testing/Received_Data/plot.py
@@ -17,7 +17,7 @@ win = 200  # window length
 
 #name = raw_input("Enter the filename you wish to graph (with file extension): ")
 #filename = "/home/pi/Documents/Received_Data/"+name
-filename = "/home/pi/Documents/MHP_raspicam/IMU/Testing/Received_Data/pz2_03-22-47.csv"
+filename = "/home/pi/Documents/MHP_raspicam/IMU/Testing/Received_Data/pz2_03-35-29.csv"
 graph_data = open(filename, 'r').read()
 lines = graph_data.split("\n")
 
