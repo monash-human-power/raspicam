e05d9162405f87e4a9ee3056b61fbeee1ada93b9
pdgra1 <pdgra1@student.monash.edu>
2018-01-06 17:41:44 +1100

    added hostname tag to sendcsv.sh
    Former-commit-id: 62fd3314dbaa086166f4bf86f00e2338fb2327d1

--- Accelerometers/LSM9DS1/BumpTest_Code/Shell_Scripts/sendcsv.sh
+++ Accelerometers/LSM9DS1/BumpTest_Code/Shell_Scripts/sendcsv.sh
@@ -2,7 +2,6 @@
 
 
 printf "\nSending data csv file....\n\n"
-source=$(cat /etc/hostname)
-time=$(date +%H-%M-%S)
-scp /home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Pi_Data_v2a.csv pi@patpi.local:/home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Received_Data/$source_$time.csv
+source=$(printf "$(hostname)_$(date +%H-%M-%S)")
+scp /home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Pi_Data_v2a.csv pi@patpi.local:/home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Received_Data/$source.csv
 printf "\nData sent.\n\n"
