f2c8d6ccb117a2b47f0bfcef5d3ff530380f1644
Pat Graham <pdgra1@student.monash.edu>
2018-01-06 16:33:37 +1100

    Updated Directories

--- IMU/Testing/sendcsv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-printf "Sending data csv file....\n\n"
-time=$(date +%H-%M-%S)
-scp /home/pi/Documents/MHP_raspicam/IMU/Testing/Test_Data/Pi_Back.csv pi@patpi.local:/home/pi/Documents/MHP_raspicam/IMU/Testing/Received_Data/pz2_$time.csv
-printf "\nData sent.\n\n"
-
-exit 0
