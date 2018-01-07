8ac281b36b9385666978cc206a864b0b555244f8
pdgra1 <pdgra1@student.monash.edu>
2018-01-07 16:52:30 +1100

    Added button_LED.py code

---
+++ Accelerometers/LSM9DS1/Other_Code/GPIO_Code/Button_LED.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+	while True:
+		button_state = GPIO.input(23)
+		if button_state == False:
+			print "Button Pressed, LED on"
+			GPIO.output(18,GPIO.HIGH)
+			time.sleep(0.2)
+		else:
+			print "Button not pressed, LED off"
+			GPIO.output(18,GPIO.LOW)
+			time.sleep(0.2)
+except:
+	GPIO.cleanup()
+
