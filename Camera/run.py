74731e47d153e2f38447eac74dd08de01a489180
Pat <pdgra1@student.monash.edu>
2018-01-07 18:50:43 +1100

    fixed bugs

--- Camera/run.py
+++ Camera/run.py
@@ -1,7 +1,9 @@
+import subprocess
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -12,20 +14,24 @@ try:
             button_state = GPIO.input(24)
             if button_state == False:
                 print("\nStarting Camera Recording..")
+                recording = 1
                 break
             time.sleep(0.2)
 
         p1 = subprocess.Popen(["python", "Camera.py"])
-        sleep(1)
+        time.sleep(1)
 
         while True:
             button_state = GPIO.input(24)
             if button_state == False:
-                p1.terminate()
-                print("\n\nExecution Terminated.\n")
+                subprocess.Popen.kill(p1)
+                recording = 0
+                print("\n\nStopping Camera Recording...\n")
+                time.sleep(3)
                 break
             time.sleep(0.2)
 except KeyboardInterrupt:
-    p1.terminate()
+    if recording == 1:
+        subprocess.Popen.kill(p1)
     print("\n\nProgram Ended.\n")
     GPIO.cleanup()
