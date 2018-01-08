5c4f9769a3da0e31f98741e7e667776c5319608a
Pat Graham <pdgra1@student.monash.edu>
2018-01-08 13:25:02 +1100

    Added pushbutton run script
    Former-commit-id: e1dc7493f77055045da77204867eed0cfc78974d

--- Camera/run.py
+++ Camera/run.py
@@ -1,11 +1,18 @@
+# ---------------------------------------------------------------------
+# Last Modified:
+#   8-1-2018
+# Description:
+#   This program is the camera master. It responds to a pushbutton input
+#   to start and stop camera recording.
+# ---------------------------------------------------------------------
+
 import subprocess
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(18, GPIO.OUT)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
@@ -13,23 +20,23 @@ try:
         while True:
             button_state = GPIO.input(24)
             if button_state == False:
-                print("\nStarting Camera Recording..")
+                print("\nStarting Camera Recording...")
                 recording = 1
                 break
-            time.sleep(0.2)
+            sleep(0.2)
 
         p1 = subprocess.Popen(["python", "Camera.py"])
-        time.sleep(1)
+        sleep(1)
 
         while True:
             button_state = GPIO.input(24)
             if button_state == False:
+                print("\n\nStopping Camera Recording...\n")
                 subprocess.Popen.kill(p1)
                 recording = 0
-                print("\n\nStopping Camera Recording...\n")
                 time.sleep(3)
                 break
-            time.sleep(0.2)
+            sleep(0.2)
 except KeyboardInterrupt:
     if recording == 1:
         subprocess.Popen.kill(p1)
