2d047a2aba9195fafe07b69b6108acd1e9b1ee03
Pat <pdgra1@student.monash.edu>
2018-01-09 20:54:29 +1100

    Updated Camera code
    Former-commit-id: cd46cdac93390e1329fd8133bd31e603c9e06824

--- Camera/run.py
+++ Camera/run.py
@@ -13,14 +13,20 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+convert = False
+rec_num = 0
+recording = 0
+
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
+    print("")
     while True:
-        while True:
+        print("Ready!\n")
+	while True:
             button_state = GPIO.input(24)
             if button_state == False:
-                print("\nStarting Camera Recording...")
+                print("Starting Camera Recording...\n")
                 recording = 1
                 break
             sleep(0.2)
@@ -31,11 +37,15 @@ try:
         while True:
             button_state = GPIO.input(24)
             if button_state == False:
-                print("\n\nStopping Camera Recording...\n")
+                print("\nStopping Camera Recording...\n\n")
                 subprocess.Popen.kill(p1)
                 recording = 0
-		#subprocess.Popen()
-                sleep(3)
+		if convert == True:
+			subprocess.call(["bash","/home/pi/Documents/MHP_raspicam/Camera/convert.sh",str(rec_num)])
+                	rec_num = rec_num + 1
+			print("")
+		print("Wait....\n")
+		sleep(1)
                 break
             sleep(0.2)
 except KeyboardInterrupt:
