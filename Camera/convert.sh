cd46cdac93390e1329fd8133bd31e603c9e06824
Pat <pdgra1@student.monash.edu>
2018-01-09 20:54:29 +1100

    Updated Camera code

--- Camera/convert.sh
+++ Camera/convert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -vcodec copy /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.mkv
+ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.mkv
