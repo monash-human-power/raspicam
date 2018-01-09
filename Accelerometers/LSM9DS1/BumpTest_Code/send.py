a65e3b12034298f3390d526cab15487a3c4d730e
Pat Graham <pdgra1@student.monash.edu>
2018-01-09 21:12:42 +1100

    added send/receive code
    Former-commit-id: 6009734b7ab6e521f3319668db75383c12cb614f

---
+++ Accelerometers/LSM9DS1/BumpTest_Code/send.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP = "PatPi.local"  # CHANGE TO DESTINATION IP ADDRESS
+UDP_PORT = 5005
+
+MESSAGE = "Hello!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
