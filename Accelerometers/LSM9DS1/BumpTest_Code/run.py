24f0558294b2dff24fa60bac703c637b251be9c5
Pat Graham <pdgra1@student.monash.edu>
2018-01-10 17:42:33 +1100

    Added basic run code
    Former-commit-id: dd222ff76a05b907cb1d5139763365aaeaf2c564

---
+++ Accelerometers/LSM9DS1/BumpTest_Code/run.py
@@ -0,0 +1,29 @@
+# ---------------------------------------------------------------------
+# Last Modified:
+#   30-12-2017
+# Description:
+#   This program is the test master. It decides which modules run append
+#   and in what order.
+# ---------------------------------------------------------------------
+
+import subprocess
+from time import sleep
+
+send = 0
+
+p1 = subprocess.Popen(["python", "datav2a.py"])
+print("\nStarting Data Recording v2a...")
+sleep(1)
+
+while True:
+    try:
+        sleep(1)
+    except KeyboardInterrupt:
+        p1.terminate()
+        print("\n\nExecution Terminated.\n")
+        break
+
+if send == 1:
+    p3 = subprocess.call(
+        '/home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Shell_Scripts/sendcsv.sh')
+    sleep(1)
