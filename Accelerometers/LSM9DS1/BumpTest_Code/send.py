08a581c97e31d377edd3ef6861102212f0cf1ff9
Pat <pdgra1@student.monash.edu>
2018-01-10 13:37:23 +1100

    Fined tuned send.py
    Former-commit-id: 3061c4c35e2551b22b4aba8ddd259c1f8369bd3a

--- Accelerometers/LSM9DS1/BumpTest_Code/send.py
+++ Accelerometers/LSM9DS1/BumpTest_Code/send.py
@@ -4,12 +4,12 @@ import subprocess
 import socket
 
 PatPiMAC='b8:27:eb:d0:ac:d8'
-PatMacbookMAC='6c:40:08:99:ce:b0'
+#PatMacbookMAC='6c:40:08:99:ce:b0'
 PiZero1MAC='b8:27:eb:b3:52:f'
 PiZero2MAC='b8:27:eb:8d:cd:e5'
 PiZero3MAC='b8:27:eb:3b:26:bb'
 
-MAC_List = [PatPiMAC,PatMacbookMAC,PiZero1MAC,PiZero2MAC,PiZero3MAC]
+MAC_List = [PatPiMAC,PiZero1MAC,PiZero2MAC,PiZero3MAC]
 
 index = 0
 Online_List=[0] * len(MAC_List)
@@ -20,6 +20,9 @@ for MAC in MAC_List:
     index = index + 1
 
 for IP in Online_List:
+    if IP == '0':
+        continue
+    
     UDP_IP = IP
     UDP_PORT = 5005
     
@@ -30,5 +33,5 @@ for IP in Online_List:
     print "message:", MESSAGE
     
     sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
+                 socket.SOCK_DGRAM)  # UDP
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
