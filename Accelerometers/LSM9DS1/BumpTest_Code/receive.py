b3031ea00ec5c8bad881a7a609b2d15e63a5e62a
pdgra1 <pdgra1@student.monash.edu>
2018-01-10 17:14:43 +1100

    Improved receive.py code

--- Accelerometers/LSM9DS1/BumpTest_Code/receive.py
+++ Accelerometers/LSM9DS1/BumpTest_Code/receive.py
@@ -7,17 +7,18 @@ UDP_IP = subprocess.check_output(['hostname', '-I'])
 UDP_PORT = 5005
 start = 1
 
-try:
-    print ""
-    while True:
-        sock = socket.socket(socket.AF_INET,  # Internet
+sock = socket.socket(socket.AF_INET,  # Internet
                              socket.SOCK_DGRAM)  # UDP
-        sock.bind((UDP_IP, UDP_PORT))
+sock.bind((UDP_IP, UDP_PORT))
 
+print ""
+try:
+    while True:
+        print "Ready!\n"
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
 
         if start == 1:
-            print "Received Start Command!\n"
+            print "Received Start Command!"
             start = 0
             p1 = subprocess.Popen(["python", "datav2a.py"])
             sleep(1)
@@ -26,9 +27,15 @@ try:
             print "Received Stop Command!\n"
             start = 1
             subprocess.Popen.kill(p1)
-            print("\n\nExecution Terminated.\n")
+            print("Execution Terminated.")
             p3 = subprocess.call(
                 '/home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Shell_Scripts/sendcsv.sh')
             sleep(1)
 except KeyboardInterrupt:
-    print("\n\nProgram Ended.\n")
+    if start == 0:
+        subprocess.Popen.kill(p1)
+        print("\n\nExecution Terminated.")
+        p3 = subprocess.call('/home/pi/Documents/MHP_raspicam/Accelerometers/LSM9DS1/BumpTest_Code/Shell_Scripts/sendcsv.sh')
+        print("Program Ended.\n")
+    else:
+        print("\n\nProgram Ended.\n")
