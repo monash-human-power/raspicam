6d28232c06878d86cd437b05368f144435ac4547
Pat <pdgra1@student.monash.edu>
2018-01-17 11:35:28 +1100

    Update Camera ode for pushbutton circuit

--- Accelerometers/LSM9DS1/Other_Code/GPIO_Code/LED.py
+++ Accelerometers/LSM9DS1/Other_Code/GPIO_Code/LED.py
@@ -3,9 +3,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
 print "LED on"
-GPIO.output(18,GPIO.HIGH)
+GPIO.output(24,GPIO.HIGH)
 time.sleep(1)
 print "LED off"
-GPIO.output(18,GPIO.LOW)
+GPIO.output(24,GPIO.LOW)
