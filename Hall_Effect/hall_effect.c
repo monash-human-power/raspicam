da4ad16f0a3185cc7283434824b270e5cffab0bb
Pat Graham <pdgra1@student.monash.edu>
2018-02-26 23:08:22 +1100

    altered hall effect detection code
    Former-commit-id: acb9af1151be8ddf03cf1c3258c5596635a5e38f

--- Hall_Effect/hall_effect.c
+++ Hall_Effect/hall_effect.c
@@ -1,6 +1,11 @@
 #include <pigpio.h>
 #include <stdio.h>
 
+void aFunction(int gpio, int level, uint32_t tick)
+{
+   printf("GPIO %d became %d at %d", gpio, level, tick);
+}
+
 int main()
 {
     if (gpioInitialise() < 0)
@@ -9,16 +14,11 @@ int main()
         return 1;
     }
 
-    void aFunction(int gpio, int level, uint32_t tick)
+    while(1)
     {
-       printf("GPIO %d became %d at %d", gpio, level, tick);
+        // call aFunction whenever GPIO 4 changes state
+        gpioSetAlertFunc(4, aFunction);
     }
-
-    // call aFunction whenever GPIO 4 changes state
-
-    gpioSetAlertFunc(4, aFunction);
-
     gpioTerminate();
-
     return 1;
 }
