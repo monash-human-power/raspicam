218793603a8b774a0b4098f247135946914e4ebc
pdgra1 <pdgra1@student.monash.edu>
2018-02-26 23:27:55 +1100

    Created detection script
    Former-commit-id: 375ba32410d83862a67875a72be0d184a805cc1a

--- Hall_Effect/hall_effect.c
+++ Hall_Effect/hall_effect.c
@@ -1,13 +1,17 @@
 #include <pigpio.h>
 #include <stdio.h>
 
+double start;
+
 void aFunction(int gpio, int level, uint32_t tick)
 {
-   printf("GPIO %d became %d at %d", gpio, level, tick);
+   printf("\nGPIO %d became %d at %.2f\n", gpio, level, time_time()-start);
 }
 
 int main()
 {
+    start = time_time();
+
     if (gpioInitialise() < 0)
     {
         printf("Failed to initialize GPIO!");
@@ -18,6 +22,7 @@ int main()
     {
         // call aFunction whenever GPIO 4 changes state
         gpioSetAlertFunc(4, aFunction);
+	time_sleep(0.1);
     }
     gpioTerminate();
     return 1;
