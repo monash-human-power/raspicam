48d81f8539d159b55f19481f65679aaad3b56494
Pat Graham <pdgra1@student.monash.edu>
2018-02-26 22:43:26 +1100

    altered hall effect detection code

--- Hall_Effect/hall_effect.c
+++ Hall_Effect/hall_effect.c
@@ -1,18 +1,23 @@
 #include <pigpio.h>
 
-if (gpioInitialise() < 0)
+int main()
 {
-    printf("Failed to initialize GPIO!");
+    if (gpioInitialise() < 0)
+    {
+        printf("Failed to initialize GPIO!");
+        return 1;
+    }
+
+    void aFunction(int gpio, int level, uint32_t tick)
+    {
+       printf("GPIO %d became %d at %d", gpio, level, tick);
+    }
+
+    // call aFunction whenever GPIO 4 changes state
+
+    gpioSetAlertFunc(4, aFunction);
+
+    gpioTerminate();
+
     return 1;
 }
-
-void aFunction(int gpio, int level, uint32_t tick)
-{
-   printf("GPIO %d became %d at %d", gpio, level, tick);
-}
-
-// call aFunction whenever GPIO 4 changes state
-
-gpioSetAlertFunc(4, aFunction);
-
-gpioTerminate();
