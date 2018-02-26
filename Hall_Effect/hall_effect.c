ac8039657c6ee83b3e1ac89e0e45597a334d0413
Pat Graham <pdgra1@student.monash.edu>
2018-02-26 22:40:20 +1100

    added hall effect detectionc ode
    Former-commit-id: b1d778a9093a2da7919a47a4a563512e0e4e76d5

---
+++ Hall_Effect/hall_effect.c
@@ -0,0 +1,18 @@
+#include <pigpio.h>
+
+if (gpioInitialise() < 0)
+{
+    printf("Failed to initialize GPIO!");
+    return 1;
+}
+
+void aFunction(int gpio, int level, uint32_t tick)
+{
+   printf("GPIO %d became %d at %d", gpio, level, tick);
+}
+
+// call aFunction whenever GPIO 4 changes state
+
+gpioSetAlertFunc(4, aFunction);
+
+gpioTerminate();
