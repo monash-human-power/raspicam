0b187d6a05e01e9fe01193e8f5005f6f32157167
Pat Graham <pdgra1@student.monash.edu>
2018-02-28 23:00:03 +1100

    added a comment

---
+++ Accelerometers/LSM6DS3/Documentation/Support_Code/spi_test.c
@@ -0,0 +1,43 @@
+#include <pigpio.h>
+#include <stdio.h>
+
+#define WHO_AM_I 0x0f
+#define READ_BIT 0x80   // 0b10000000
+
+int readBytes(int handle, char *data, int count)
+{
+    data[0] |= READ_BIT;
+    return spiXfer(handle, data, data, count);
+}
+
+int main()
+{
+    int LSM6DS3;
+    char data[7]; // comments
+    const int baud = 5000000;  // SPI baudrate
+    int count=0;
+
+    if (gpioInitialise() < 0)
+    {
+        printf("\nFailed to initialize GPIO!\n\n");
+        return 1;
+    }
+
+    LSM6DS3 = spiOpen(0, baud, 3);
+    if (LSM6DS3 < 0)
+    {
+        printf("spiOpen Failed.\n\n");
+    }
+
+    //count=spiXfer(LSM6DS3,WHO_AM_I,result,1);
+    data[0]=WHO_AM_I;
+    count = readBytes(LSM6DS3, data, 1);
+
+    printf("\n%d bytes were transferred\n\n",count);
+
+    if(data[1]==0x69)
+    {
+        printf("\nCommunication Successful!\n\n");
+    }
+    return 1;
+}
