291c794e33c2285d5622b70155f8ef06ff3cca69
Pat Graham <pdgra1@student.monash.edu>
2018-03-03 23:48:46 +1100

    added reed switch code

---
+++ Hall_Effect/ReedSwitch.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        button_state = GPIO.input(24)
+        if button_state == False:
+            print "Switch closed."
+        else:
+            print "Switch open."
+        time.sleep(0.01)
+except:
+    GPIO.cleanup()
