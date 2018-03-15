eaab4fcb44870baa6f1489a81df23e0deb3c7e4b
Pat Graham <pdgra1@student.monash.edu>
2018-03-15 22:06:40 +1100

    Code is working?

--- Accelerometers/H3LIS331DL/Documentation/Support_Code/spi_test.c
+++ Accelerometers/H3LIS331DL/Documentation/Support_Code/spi_test.c
@@ -8,7 +8,7 @@
 // ACCELEROMETER REGISTERS
 #define CTRL_REG1   0x20
 #define CTRL_REG4   0x23
-#define DATAX0      0x0F//28
+#define DATAX0      0x28
 
 // ACCELEROMETER SETTINGS
 #define CTRL_REG1_CONTENTS 0b00111111   // Normal Mode, 1000Hz ODR, Enable axes
