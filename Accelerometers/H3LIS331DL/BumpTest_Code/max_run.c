aa5b45ef8e2a0a3c6531bd428293cbcea86b1484
Pat Graham <pdgra1@student.monash.edu>
2018-03-27 19:08:45 +1100

    added max_run
    Former-commit-id: 4799e9557f32593c8a3ad273c998fa946d61a6ea

---
+++ Accelerometers/H3LIS331DL/BumpTest_Code/max_run.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pigpio.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define READ_BIT      0x80
+#define MULTI_BIT     0x40
+#define CTRL_REG1   0x20
+#define CTRL_REG4   0x23
+#define DATAX0      0x28
+#define CTRL_REG1_CONTENTS 0b00111111   // Normal Mode, 1000Hz ODR, Enable axes
+#define CTRL_REG4_CONTENTS 0b00000000   // +-100g (change bits 2/3 - 00 = 100g, 01 = 200g, 11 = 400g)
+
+int readBytes(int handle, char *data, int count) {
+    data[0] |= READ_BIT;
+    if (count > 1) data[0] |= MULTI_BIT;
+    return spiXfer(handle, data, data, count);
+}
+
+int writeBytes(int handle, char *data, int count) {
+    if (count > 1) data[0] |= MULTI_BIT;
+    return spiWrite(handle, data, count);
+}
+
+int main(int argc, char *argv[])
+{
+    h = spiOpen(0, speedSPI, 3);
+    data[0] = CTRL_REG1;
+    data[1] = CTRL_REG1_CONTENTS;
+    writeBytes(h, data, 2);
+    data[0] = CTRL_REG4;
+    data[1] = CTRL_REG4_CONTENTS;
+
+    tStart = time_time();
+    while (1)
+    {
+        data[0] = DATAX0;
+        bytes = readBytes(h, data, 7);
+        if (bytes == 7)
+        {
+            x = (data[2]<<8)|data[1] * accConversion;
+            y = (data[4]<<8)|data[3] * accConversion;
+            z = (data[6]<<8)|data[5] * accConversion;
+            t = time_time() - tStart;
+
+            printf("\n");
+            if (x > 30 | y > 30 | z > 30)
+            {
+                printf("t = %.2f\tx = %.2f\ty = %.2f\tz = %.2f\n");
+            }
+        }
+        else {
+            success = 0;
+        }
+        time_sleep(delay);  // pigpio sleep is accurate enough for console output, not necessary to use nanosleep
+    }
+    gpioTerminate();
