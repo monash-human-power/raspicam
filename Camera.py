021fe7e9ea76850ee83ac52ee06a47e1635bb100
pdgra1 <pdgra1@student.monash.edu>
2018-06-16 17:25:35 +1000

    Fixed Mistake
    Former-commit-id: b9e1f3165ab085ee6d41726a6b0ed4b9a61f4bc0

--- Camera.py
+++ Camera.py
@@ -20,11 +20,11 @@ textPad2 = Image.new('RGB', (128, 64), "#298")
 textPadImage2 = textPad2.copy()
 
 j = 0
-while os.path.exists("/home/pi/Documents/MHP_raspicam/Video/Recording_%s.h264" % j):
+while os.path.exists("/home/pi/Documents/MHP_Raspicam/Video/Recording_%s.h264" % j):
     j += 1
 
 #filename = "video" + str(j) + + ".h264"
-filename = "/home/pi/Documents/MHP_raspicam/Video/Recording_%s.h264" % j
+filename = "/home/pi/Documents/MHP_Raspicam/Video/Recording_%s.h264" % j
 
 i = 0
 with picamera.PiCamera() as camera:
