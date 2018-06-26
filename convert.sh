d8e7a1b5e2dcaf8f3cf38daba825f0e13063be06
pdgra1 <pdgra1@student.monash.edu>
2018-06-26 20:23:02 +1000

    Updated code with latest working version, sorry minh!
    Former-commit-id: 2eb2f0be204cd557ab209e69a2216b2b3d2ff76a

--- convert.sh
+++ convert.sh
@@ -5,9 +5,9 @@ now=$(date +"RUN_%d_%m_@_%H-%M-%S")
 #ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_$1.h264 -vcodec copy /home/pi/Documents/MHP_raspicam/Camera/Video/$now.mkv
 
 # Primary
-ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -vcodec copy /media/pi/PRIMARY/$now.mkv
+ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec copy /media/pi/PRIMARY/$now.mkv
 
 # Secondary
-ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -vcodec copy /media/pi/SECONDARY/$now.mkv
+ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec copy /media/pi/SECONDARY/$now.mkv
 
-sudo rm /home/pi/Documents/MHP_raspicam/Camera/Video/*.h264
+sudo rm /home/pi/Documents/MHP_Raspicam/Video/*.h264
