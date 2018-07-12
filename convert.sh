f9ae059a70b4fca52f7eca89a7862d354d9f2d74
Pat Graham <pdgra1@student.monash.edu>
2018-07-12 21:55:44 +1000

    Added some extra functionality to move csv files

--- convert.sh
+++ convert.sh
@@ -19,13 +19,9 @@ ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -v
 # If the display is the secondary run this command. This command will fail if the display is the primary or no usb is plugged in.
 ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -vcodec copy /media/pi/SECONDARY/$now.mkv
 
-# Remove .h264 files from the SD Card
+# Delete video files after conversion
 sudo rm /home/pi/Documents/MHP_Raspicam/Video/*.h264
 
-# Primary
-ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec copy /media/pi/PRIMARY/$now.mkv
-
-# Secondary
-ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec copy /media/pi/SECONDARY/$now.mkv
-
-sudo rm /home/pi/Documents/MHP_Raspicam/Video/*.h264
+# Move csv files for Reed Switch to USB if present
+sudo mv /home/pi/Documents/MHP_Raspicam/ReedSwitch/*.csv /media/pi/PRIMARY/$now.csv
+sudo mv /home/pi/Documents/MHP_Raspicam/ReedSwitch/*.csv /media/pi/SECONDARY/$now.csv
