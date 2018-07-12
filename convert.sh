fb7e9089eaf84db2dbcbd7df37f0b30d86485934
Pat Graham <pdgra1@student.monash.edu>
2018-07-12 21:48:59 +1000

    fixed up errors introduced from previous commits

--- convert.sh
+++ convert.sh
@@ -13,7 +13,6 @@
 # Get the current date and time
 now=$(date +"RUN_%d_%m_@_%H-%M-%S")
 
-<<<<<<< HEAD
 # If the display is the primary run this command. This command will fail if the display is the secondary or no usb is plugged in.
 ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -vcodec copy /media/pi/PRIMARY/$now.mkv
 
@@ -22,7 +21,7 @@ ffmpeg -r 45 -i /home/pi/Documents/MHP_raspicam/Camera/Video/Recording_0.h264 -v
 
 # Remove .h264 files from the SD Card
 sudo rm /home/pi/Documents/MHP_Raspicam/Video/*.h264
-=======
+
 # Primary
 ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec copy /media/pi/PRIMARY/$now.mkv
 
@@ -30,4 +29,3 @@ ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec c
 ffmpeg -r 45 -i /home/pi/Documents/MHP_Raspicam/Video/Recording_0.h264 -vcodec copy /media/pi/SECONDARY/$now.mkv
 
 sudo rm /home/pi/Documents/MHP_Raspicam/Video/*.h264
->>>>>>> dbf10f4f776629d6afcc24f529c47f4f328739bc
