376cccfa00dbfaac93534465a9336638b07b33d7
khanguslee <angus.lee809@gmail.com>
2019-03-19 20:46:45 +1100

    Draw text everytime new data comes in

--- Documentation/Development_Code/Overlay_Demo.py
+++ Documentation/Development_Code/Overlay_Demo.py
@@ -14,10 +14,21 @@ import datetime as dt
 import paho.mqtt.client as mqtt
 import json
 
+text_height = 20
+text_font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf',text_height)
+
 GLOBAL_DATA = {
-    power: 0,
-    cadence: 0,
+    "power": 0,
+    "cadence": 0,
 }
+
+# The resolution of the camera preview. Current system using 800x480.
+WIDTH = 800
+HEIGHT = 480
+
+# Initiate camera preview
+camera = PiCamera(resolution=(WIDTH, HEIGHT))
+camera.start_preview()
 
 # Convert data to a suitable format
 def parse_data(data):
@@ -38,24 +49,26 @@ def on_connect(client, userdata, rc):
 
 def on_message(client, userdata, msg):
     if msg.topic == "data":
+        print(data)
         data = str(msg.payload.decode("utf-8"))
         parsed_data = parse_data(data)
         GLOBAL_DATA.power = parsed_data.power
         GLOBAL_DATA.cadence = parsed_data.cadence
+        # Display power
+        draw.text((10, 10 + text_height*1), str(GLOBAL_DATA["power"]), font=text_font, fill='black')
+
+        # Display cadence
+        draw.text((10, 10 + text_height*2), str(GLOBAL_DATA["cadence"]), font=text_font, fill='black')
+
+        # Add the image to the preview overlay
+        overlay = camera.add_overlay(img.tobytes(), format='rgba', size=img.size)
+        overlay.layer = 3
+        overlay.fullscreen = True
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect("192.168.100.100", 1883, 60)
-
-
-# The resolution of the camera preview. Current system using 800x480.
-WIDTH = 800
-HEIGHT = 480
-
-# Initiate camera preview
-camera = PiCamera(resolution=(WIDTH, HEIGHT))
-camera.start_preview()
 
 # Create a transparent image to attach text
 img = Image.new('RGBA', (WIDTH, HEIGHT))
@@ -81,20 +94,10 @@ draw.text((WIDTH/2, HEIGHT-unit_height), unit, font=unit_font, fill='black')
 Text display for power, cadence (pedalling rate), distance, heart rate. As mentionaed
 above, these are dummy text for displaying purpose only.
 """
-text_font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf',text_height)
-text_height = 20
+
 #display_text = ['Pwr: {}'.format(0),'Cad: {}'.format(0.0), 'Dist: {}'.format(0.0), 'H/r: {}'.format(0)]
 
-# Display power
-draw.text((10, 10 + text_height*1), GLOBAL_DATA.power, font=text_font, fill='black')
 
-# Display cadence
-draw.text((10, 10 + text_height*2), GLOBAL_DATA.cadence, font=text_font, fill='black')
-
-# Add the image to the preview overlay
-overlay = camera.add_overlay(img.tobytes(), format='rgba', size=img.size)
-overlay.layer = 3
-overlay.fullscreen = True
 
 
 """
@@ -107,9 +110,9 @@ camera.annotate_text_size = 26
 camera.annotate_foreground = Color('black')
 
 # Update the time after 1 second
-while True:
-    sleep(1)
-    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+# while True:
+#     sleep(1)
+#     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 # mqtt loop
 client.loop_forever()
