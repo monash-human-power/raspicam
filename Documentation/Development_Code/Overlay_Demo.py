cc71de94470e3f5192d890c4616b560ea4046a5b
khanguslee <khanguslee@gmail.com>
2019-03-20 23:53:13 +1100

    Check power and cadence is zero before dividing

--- Documentation/Development_Code/Overlay_Demo.py
+++ Documentation/Development_Code/Overlay_Demo.py
@@ -28,6 +28,11 @@ GLOBAL_DATA = {
     "count": 0,
 }
 
+REQUIRED_DATA = {
+    "rec_power": 0,
+    "rec_speed": 0,
+}
+
 # The resolution of the camera preview. Current system using 800x480.
 WIDTH = 800
 HEIGHT = 480
@@ -57,6 +62,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("start")
     client.subscribe("data")
     client.subscribe("stop")
+    client.subscribe("power_model/targets")
     
     # Add static text
     img = Image.new('RGBA', (WIDTH, HEIGHT))
@@ -73,38 +79,68 @@ def on_message(client, userdata, msg):
     global START_TIME
     print(msg.topic + " " + str(msg.payload.decode("utf-8")))
     current_time = round(time.time(), 2)
+    if msg.topic == "power_model/targets":
+        req_data = str(msg.payload.decode("utf-8"))
+        parsed_data = parse_data(req_data)
+        REQUIRED_DATA["rec_power"] = int(parsed_data["rec_power"])
+        REQUIRED_DATA["rec_speed"] = int(parsed_data["rec_speed"])
+        
     if msg.topic == "data":
         data = str(msg.payload.decode("utf-8"))
-        print(data)
         parsed_data = parse_data(data)
+        print(str(parsed_data))
         GLOBAL_DATA["power"] += int(parsed_data["power"])
         GLOBAL_DATA["cadence"] += int(parsed_data["cadence"])
-        GLOBAL_DATA["gps_speed"] += float(parsed_data["gps_speed"])
+        if int(parsed_data["gps"]) == 1:
+            GLOBAL_DATA["gps_speed"] += float(parsed_data["gps_speed"])
         GLOBAL_DATA["count"] = GLOBAL_DATA["count"] + 1
         total_time = current_time - START_TIME
-        print(total_time)
         update_time = 0.5
         if total_time >= update_time:
             START_TIME = current_time
             # Create a transparent image to attach text
             img = Image.new('RGBA', (WIDTH, HEIGHT))
             draw = ImageDraw.Draw(img)
+            
             # Display power
-            power = GLOBAL_DATA["power"]/GLOBAL_DATA["count"]
-            draw.text((120, 10 + text_height*1), "{0}".format(round(power, 2)), font=text_font, fill='black')
+            if GLOBAL_DATA["power"] != 0:
+                power = GLOBAL_DATA["power"]/GLOBAL_DATA["count"]
+                rec_power = REQUIRED_DATA["rec_power"]
+                tolerance = 0.05
+                if power> rec_power and power < (rec_power + (rec_power*tolerance)):
+                    draw.text((120, 10 + text_height*1), "{0}".format(round(power, 2)), font=text_font, fill='green')
+                    
+                elif power > (rec_power + (rec_power*tolerance)):
+                    draw.text((120, 10 + text_height*1), "{0}".format(round(power, 2)), font=text_font, fill='red')
+
+                else:
+                    draw.text((120, 10 + text_height*1), "{0}".format(round(power, 2)), font=text_font, fill='black')
 
             # Display cadence
-            cadence = power = GLOBAL_DATA["cadence"]/GLOBAL_DATA["count"]
-            draw.text((120, 10 + text_height*2), "{0}".format(round(cadence, 2)), font=text_font, fill='black')
+            if GLOBAL_DATA["cadence"] != 0:
+                cadence = GLOBAL_DATA["cadence"]/GLOBAL_DATA["count"]
+                draw.text((120, 10 + text_height*2), "{0}".format(round(cadence, 2)), font=text_font, fill='black')
 
             # Display speed
-            speed_font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf',speed_height)
-            speed = GLOBAL_DATA["gps_speed"]/GLOBAL_DATA["count"]
-            speed_text = "{0} km/h".format(round(speed, 2))
-            draw.text((WIDTH/2 - 30, HEIGHT-speed_height), speed_text, font=speed_font, fill='black')
-            # Add the image to the preview overlay
+            if int(parsed_data["gps"]) == 1:
+                if GLOBAL_DATA["gps_speed"] != 0:
+                    speed_font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSans.ttf',speed_height)
+                    speed = GLOBAL_DATA["gps_speed"]/GLOBAL_DATA["count"]
+                    rec_speed = REQUIRED_DATA["rec_speed"]
+                    speed_text = "{0} km/h".format(round(speed, 2))
+                     
+                    if speed> rec_speed and speed < (rec_speed + (rec_speed*tolerance)):
+                        draw.text((WIDTH/2 - 30, HEIGHT-speed_height), speed_text, font=speed_font, fill='green')
+                        
+                    elif speed > (rec_speed + (rec_speed*tolerance)):
+                        draw.text((WIDTH/2 - 30, HEIGHT-speed_height), speed_text, font=speed_font, fill='red')
+
+                    else:
+                        draw.text((WIDTH/2 - 30, HEIGHT-speed_height), speed_text, font=speed_font, fill='black')
+
+
+            # Remove and add the image to the preview overlay
             global PREV_OVERLAY
-            print(PREV_OVERLAY)
             if PREV_OVERLAY:
                 camera.remove_overlay(PREV_OVERLAY)
             overlay = camera.add_overlay(img.tobytes(), format='rgba', size=img.size)
