a8b43176239cbdfcf106c8680b5535385dedd65d
harsilspatel <harsilspatel@gmail.com>
2019-07-31 11:11:15 +1000

    implemented methods to retrieve the active overlay

--- CameraOverlay/commons.py
+++ CameraOverlay/commons.py
@@ -2,6 +2,8 @@ import os
 import json
 import fnmatch
 
+CONFIG_FILE = 'configs.json'
+ACTIVE_OVERLAY_KEY = 'activeOverlay'
 OVERLAY_FILE_PATTERN = 'Overlay_Demo*.py'
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,10 +14,16 @@ def get_overlays(dir=CURRENT_DIRECTORY):
 
 
 def set_overlay(overlay, dir=CURRENT_DIRECTORY):
-	with open(os.path.join(dir, 'configs.json'), 'w') as f:
-		json.dump({'activeOverlay': overlay}, f, indent=2)
+	with open(os.path.join(dir, CONFIG_FILE), 'w') as f:
+		json.dump({ACTIVE_OVERLAY_KEY: overlay}, f, indent=2)
 
+def get_active_overlay(dir=CURRENT_DIRECTORY):
+	with open(os.path.join(dir, CONFIG_FILE)) as f:
+		data = json.load(f)
+		active_overlay = data[ACTIVE_OVERLAY_KEY]
+		return os.path.join(dir, active_overlay)
 
 if __name__ == '__main__':
 	print(get_overlays())
-	set_overlay('test')
+	set_overlay('bleh bleh bleh')
+	print(get_active_overlay())
