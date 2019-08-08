edb968bce83f19b9ba115565b97559aa2b4ae7f9
harsilspatel <harsilspatel@gmail.com>
2019-08-08 16:23:04 +1000

    categorise topics according to the devices

--- CameraOverlay/topics.py
+++ CameraOverlay/topics.py
@@ -1,8 +1,16 @@
 from enum import Enum
 
-class Topics(Enum):
+class DAS(Enum):
 	data = 'data'
-	start = 'start',
+	start = 'start'
 	stop = 'stop'
-	power_model_max_speed = 'power_model/max_speed',
-	power_model_recommended_sp = 'power_model/recommended_SP'
+
+class PowerModel(Enum):
+	max_speed = 'power_model/max_speed',
+	recommended_sp = 'power_model/recommended_SP'
+
+class Camera(Enum):
+	get_overlays = 'camera/get_overlays'
+	set_overlays = 'camera/set_overlays'
+	push_overlays = 'camera/push_overlays'
+
