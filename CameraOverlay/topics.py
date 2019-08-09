c642074038d333bfccdf5d1e0853306f3f1ef532
harsilspatel <harsilspatel@gmail.com>
2019-08-10 00:09:23 +1000

    implement NeatEnum

--- CameraOverlay/topics.py
+++ CameraOverlay/topics.py
@@ -1,16 +1,28 @@
-from enum import Enum
+from enum import Enum, unique
 
-class DAS(Enum):
+@unique
+class NeatEnum(Enum):
+	def __str__(self):
+		return self.value[0]
+
+
+class DAS(NeatEnum):
 	data = 'data'
 	start = 'start'
 	stop = 'stop'
 
-class PowerModel(Enum):
+
+class PowerModel(NeatEnum):
 	max_speed = 'power_model/max_speed',
 	recommended_sp = 'power_model/recommended_SP'
 
-class Camera(Enum):
+
+class Camera(NeatEnum):
 	get_overlays = 'camera/get_overlays'
 	set_overlays = 'camera/set_overlays'
 	push_overlays = 'camera/push_overlays'
 
+
+if __name__ == '__main__':
+	x = PowerModel.max_speed
+	print()
