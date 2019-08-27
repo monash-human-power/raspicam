f68169e91d621fd1a03a9cdd79392176c8dc3c6f
harsilspatel <harsilspatel@gmail.com>
2019-08-28 09:32:25 +1000

    refactor dir to directory

--- CameraOverlay/commons.py
+++ CameraOverlay/commons.py
@@ -10,26 +10,26 @@ OVERLAY_FILE_PATTERN = 'Overlay_Demo*.py'
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 
-def get_overlays(dir=CURRENT_DIRECTORY):
-	overlays = [file for file in os.listdir(dir) if fnmatch.fnmatch(file, OVERLAY_FILE_PATTERN)]
+def get_overlays(directory=CURRENT_DIRECTORY):
+	overlays = [file for file in os.listdir(directory) if fnmatch.fnmatch(file, OVERLAY_FILE_PATTERN)]
 	return overlays
 
 
-def set_overlay(new_overlays, dir=CURRENT_DIRECTORY):
+def set_overlay(new_overlays, directory=CURRENT_DIRECTORY):
 	current_device = os.getenv('MHP_CAMERA')
 	new_overlay = new_overlays[current_device]
 
-	configs = read_configs(dir)
+	configs = read_configs(directory)
 	if 'device' in configs:
 		configs.pop('device')
 	configs[ACTIVE_OVERLAY_KEY] = new_overlay
 
-	with open(os.path.join(dir, CONFIG_FILE), 'w') as f:
+	with open(os.path.join(directory, CONFIG_FILE), 'w') as f:
 		json.dump(configs, f, indent=2, sort_keys=True)
 
 
-def read_configs(dir=CURRENT_DIRECTORY):
-	filepath = os.path.join(dir, CONFIG_FILE)
+def read_configs(directory=CURRENT_DIRECTORY):
+	filepath = os.path.join(directory, CONFIG_FILE)
 	if not os.path.isfile(filepath):
 		return {}
 	else:
@@ -42,9 +42,9 @@ def read_configs(dir=CURRENT_DIRECTORY):
 			return configs
 
 
-def get_active_overlay(dir=CURRENT_DIRECTORY):
-	configs = read_configs(dir)
-	return os.path.join(dir, configs[ACTIVE_OVERLAY_KEY])
+def get_active_overlay(directory=CURRENT_DIRECTORY):
+	configs = read_configs(directory)
+	return os.path.join(directory, configs[ACTIVE_OVERLAY_KEY])
 
 
 if __name__ == '__main__':
