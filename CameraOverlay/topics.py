48dc2006cb90fd81df96f561f00a2d672145a20d
khanguslee <angus.lee809@gmail.com>
2019-09-09 22:43:27 +1000

    Make pylint happy with topics.py

--- CameraOverlay/topics.py
+++ CameraOverlay/topics.py
@@ -1,30 +1,30 @@
+"""MQTT Topics for Camera System"""
 from enum import Enum, unique
 
 @unique
 class NeatEnum(Enum):
-	def __str__(self):
-		return self.value
+    """Our base Enum class"""
+    def __str__(self):
+        return self.value
 
 
 class DAS(NeatEnum):
-	data = 'data'
-	start = 'start'
-	stop = 'stop'
+    """DAS MQTT Topics"""
+    data = 'data'
+    start = 'start'
+    stop = 'stop'
 
 
 class PowerModel(NeatEnum):
-	plan_name = 'power_model/plan_name'
-	max_speed = 'power_model/max_speed'
-	recommended_sp = 'power_model/recommended_SP'
-	predicted_max_speed = 'power_model/predicted_max_speed'
+    """Power Model MQTT Topics"""
+    plan_name = 'power_model/plan_name'
+    max_speed = 'power_model/max_speed'
+    recommended_sp = 'power_model/recommended_SP'
+    predicted_max_speed = 'power_model/predicted_max_speed'
 
 
 class Camera(NeatEnum):
-	get_overlays = 'camera/get_overlays'
-	set_overlays = 'camera/set_overlay'
-	push_overlays = 'camera/push_overlays'
-
-
-if __name__ == '__main__':
-	x = PowerModel.max_speed
-	print(x)
+    """Camera system MQTT Topics"""
+    get_overlays = 'camera/get_overlays'
+    set_overlays = 'camera/set_overlay'
+    push_overlays = 'camera/push_overlays'
