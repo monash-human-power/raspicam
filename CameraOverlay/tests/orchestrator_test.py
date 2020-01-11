b02120e8198d53ce7c91866a5ca888f37242c282
khanguslee <khanguslee@gmail.com>
2020-01-11 15:03:45 +1100

    Create tests folder

---
+++ CameraOverlay/tests/orchestrator_test.py
@@ -0,0 +1,11 @@
+"""Test script for Orchestrator class"""
+import orchestrator
+
+
+class TestOrchestrator:
+    """Orchestrator test class"""
+    @staticmethod
+    def test_class_initiation():
+        # Remove below test and add tests here!
+        assert True is True
+        pass
