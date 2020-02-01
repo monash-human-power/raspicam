6d40e3a82186942ca542d13694ade04f89f75a34
maanika <kennethmaanika@gmail.com>
2020-02-01 20:08:25 +1100

    Add get_args function

--- CameraOverlay/tests/mqtt_message_test.py
+++ CameraOverlay/tests/mqtt_message_test.py
@@ -2,3 +2,10 @@
 import argparse
 import paho.mqtt.client as mqtt
 import time
+
+def get_args(argv=None):
+    """Get arguments passed into Python script"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost", help="ip address of the host")
+    parser.add_argument("--waitTime", type=int, default=8, help="Number of seconds until next message is sent")
+    return parser.parse_args(argv)
