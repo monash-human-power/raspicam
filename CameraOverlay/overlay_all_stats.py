a4c98777091d0cfc81b3faebb556436cb92a65c9
Christopher <hallgchris@gmail.com>
2020-04-26 10:11:05 +1000

    Add functions for each data type in overlays

--- CameraOverlay/overlay_all_stats.py
+++ CameraOverlay/overlay_all_stats.py
@@ -28,63 +28,73 @@ class OverlayAllStats(Overlay):
 		self.data_canvas.clear()
 		self.message_canvas.clear()
 
-		# Display power
 		if self.data["power"] != 0:
-			power = self.data["power"]
-			rec_power = self.data["rec_power"]
-			tolerance = 0.05
-			# Display recommended power
-			self.data_canvas.draw_text("{0}".format(round(rec_power, 2)), (340, self.text_height * 1))
-			# Display power
-			if power > (rec_power + (rec_power * tolerance)):
-				power_colour = Colour.red
-			elif power > rec_power:
-				power_colour = Colour.green
-			else:
-				power_colour = Colour.black
-			self.data_canvas.draw_text("{0}".format(round(power, 2)), (340, self.text_height * 2), colour=power_colour)
+			self.draw_power_rec_power()
 
-		# Display cadence
 		if self.data["cadence"] != 0:
-			cadence = self.data["cadence"]
-			self.data_canvas.draw_text("{0}".format(round(cadence, 2)), (340, self.text_height * 3))
+			self.draw_cadence()
 
-		# Display speed
 		if self.data["reed_velocity"] != 0:
-			# Max Speed
-			max_speed = self.data["predicted_max_speed"]
-			max_speed_text = "{0} km/h".format(round(max_speed, 2))
-			max_speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 2)
-			self.data_canvas.draw_text(max_speed_text, max_speed_pos, size=2.5)
+			self.draw_max_rec_reed_velocity()
 
-			# Recommended speed
-			rec_speed = self.data["rec_speed"]
-			rec_speed_text = "{0} km/h".format(round(rec_speed, 2))
-			rec_speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 1)
-			self.data_canvas.draw_text(rec_speed_text, rec_speed_pos, size=2.5)
-
-			# Actual speed
-			speed = self.data["reed_velocity"]
-			speed_text = "{0} km/h".format(round(speed, 2))
-			speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 0)
-			tolerance = 0.05
-			if speed > (rec_speed + (rec_speed * tolerance)):
-				speed_colour = Colour.red
-			elif speed > rec_speed:
-				speed_colour = Colour.green
-			else:
-				speed_colour = Colour.black
-			self.data_canvas.draw_text(speed_text, speed_pos, colour=speed_colour, size=2.5)
-
-		# Display reed_distance (distance travelled)
 		if self.data["reed_distance"] != 0:
-			reed_distance = self.data["reed_distance"]
-			self.data_canvas.draw_text("{0}".format(round(reed_distance, 2)), (340, self.text_height * 4))
+			self.draw_distance()
 		
-		# Display messages from DAShboard
 		if self.data.has_message():
-			message = self.data.get_message()
-			self.message_canvas.draw_text(message, (190, self.text_height * 5), size=1, colour=Colour.red)
+			self.draw_messages()
+
+	def draw_power_rec_power(self):
+		power = self.data["power"]
+		rec_power = self.data["rec_power"]
+		tolerance = 0.05
+		# Display recommended power
+		self.data_canvas.draw_text("{0}".format(round(rec_power, 2)), (340, self.text_height * 1))
+		# Display power
+		if power > (rec_power + (rec_power * tolerance)):
+			power_colour = Colour.red
+		elif power > rec_power:
+			power_colour = Colour.green
+		else:
+			power_colour = Colour.black
+		self.data_canvas.draw_text("{0}".format(round(power, 2)), (340, self.text_height * 2), colour=power_colour)
+
+	def draw_cadence(self):
+		cadence = self.data["cadence"]
+		self.data_canvas.draw_text("{0}".format(round(cadence, 2)), (340, self.text_height * 3))
+
+	def draw_max_rec_reed_velocity(self):
+		# Max Speed
+		max_speed = self.data["predicted_max_speed"]
+		max_speed_text = "{0} km/h".format(round(max_speed, 2))
+		max_speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 2)
+		self.data_canvas.draw_text(max_speed_text, max_speed_pos, size=2.5)
+
+		# Recommended speed
+		rec_speed = self.data["rec_speed"]
+		rec_speed_text = "{0} km/h".format(round(rec_speed, 2))
+		rec_speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 1)
+		self.data_canvas.draw_text(rec_speed_text, rec_speed_pos, size=2.5)
+
+		# Actual speed
+		speed = self.data["reed_velocity"]
+		speed_text = "{0} km/h".format(round(speed, 2))
+		speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 0)
+		tolerance = 0.05
+		if speed > (rec_speed + (rec_speed * tolerance)):
+			speed_colour = Colour.red
+		elif speed > rec_speed:
+			speed_colour = Colour.green
+		else:
+			speed_colour = Colour.black
+		self.data_canvas.draw_text(speed_text, speed_pos, colour=speed_colour, size=2.5)
+
+	def draw_distance(self):
+		reed_distance = self.data["reed_distance"]
+		self.data_canvas.draw_text("{0}".format(round(reed_distance, 2)), (340, self.text_height * 4))
+
+	def draw_messages(self):
+		message = self.data.get_message()
+		self.message_canvas.draw_text(message, (190, self.text_height * 5), size=1, colour=Colour.red)
 
 if __name__ == '__main__':
 	args = Overlay.get_overlay_args("Overlay displaying all (or just many) statistics")
--- CameraOverlay/overlay_top_strip.py
+++ CameraOverlay/overlay_top_strip.py
@@ -37,38 +37,48 @@ class OverlayTopStrip(Overlay):
 		time_string = "{:0>2}:{:0>2}".format(int(minutes), int(seconds))
 		self.data_canvas.draw_text(time_string, (50, self.top_text_pos), colour=Colour.white)
 
-		# Display power
 		if self.data["power"] != 0:
-			power = self.data["power"]
-			rec_power = self.data["rec_power"]
-			# Display recommended power
-			self.data_canvas.draw_text("{0}".format(round(rec_power, 0)), (600, self.top_text_pos), colour=Colour.white)
-			# Display power (no colour change)
-			self.data_canvas.draw_text("P: {0}".format(round(power, 2)), (self.bottom_text_pos_x, self.bottom_text_pos_y), size=self.bottom_text_size, colour=Colour.red)
+			self.draw_power_rec_power()
 
-		# Display speed
 		if self.data["gps"] == 1:
 			if self.data["gps_speed"] != 0:
-				# Predicted max speed
-				pred_max_speed = self.data["predicted_max_speed"]
-				self.data_canvas.draw_text("{0}".format(round(pred_max_speed, 1)), (890, self.top_text_pos), colour=Colour.white)
-
-				# Actual speed (no colour change)
-				speed = self.data["gps_speed"]
-				self.data_canvas.draw_text("S: {0}".format(round(speed, 2)), (self.bottom_text_pos_x, self.bottom_text_pos_y - self.bottom_text_height), size=self.bottom_text_size, colour=Colour.red)
-
-				# Actual max speed
-				self.data_canvas.draw_text("{0}".format(int(self.actual_max(speed))), (1120, self.top_text_pos), colour=Colour.white)
+				self.draw_speed_max_speed()
 
 		# Display zone distance left (bugged)
 		if self.data["zdist"] != 0:
-			zdist_left = self.data["zdist"]
-			self.data_canvas.draw_text("{0}".format(int(zdist_left)), (360, self.top_text_pos), colour=Colour.white)
+			self.draw_zone_dist()
 
 		# Display plan name and clear after 15 secs
 		if self.data["plan_name"] != '' and time.time() - self.start_time <= 15:
-			plan_name = self.data["plan_name"]
-			self.data_canvas.draw_text(plan_name, (0, self.height - 8), colour=Colour.red)
+			self.draw_plan_name()
+
+	def draw_power_rec_power(self):
+		power = self.data["power"]
+		rec_power = self.data["rec_power"]
+		# Display recommended power
+		self.data_canvas.draw_text("{0}".format(round(rec_power, 0)), (600, self.top_text_pos), colour=Colour.white)
+		# Display power (no colour change)
+		self.data_canvas.draw_text("P: {0}".format(round(power, 2)), (self.bottom_text_pos_x, self.bottom_text_pos_y), size=self.bottom_text_size, colour=Colour.red)
+
+	def draw_speed_max_speed(self):
+		# Predicted max speed
+		pred_max_speed = self.data["predicted_max_speed"]
+		self.data_canvas.draw_text("{0}".format(round(pred_max_speed, 1)), (890, self.top_text_pos), colour=Colour.white)
+
+		# Actual speed (no colour change)
+		speed = self.data["gps_speed"]
+		self.data_canvas.draw_text("S: {0}".format(round(speed, 2)), (self.bottom_text_pos_x, self.bottom_text_pos_y - self.bottom_text_height), size=self.bottom_text_size, colour=Colour.red)
+
+		# Actual max speed
+		self.data_canvas.draw_text("{0}".format(int(self.actual_max(speed))), (1120, self.top_text_pos), colour=Colour.white)
+
+	def draw_zone_dist(self):
+		zdist_left = self.data["zdist"]
+		self.data_canvas.draw_text("{0}".format(int(zdist_left)), (360, self.top_text_pos), colour=Colour.white)
+
+	def draw_plan_name(self):
+		plan_name = self.data["plan_name"]
+		self.data_canvas.draw_text(plan_name, (0, self.height - 8), colour=Colour.red)
 
 if __name__ == '__main__':
 	args = Overlay.get_overlay_args("Shows important statistics in a bar at the top of the screen")
