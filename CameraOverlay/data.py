6d40da403788f7fee6f6f7f2d408a758eeadcc05
Christopher <hallgchris@gmail.com>
2020-05-02 22:14:28 +1000

    Add DataV2/DataV3 classes

--- CameraOverlay/data.py
+++ CameraOverlay/data.py
@@ -1,23 +1,15 @@
+from abc import ABC, abstractmethod
 import time
 from typing import Any, Optional
 import topics as topics
 
-V2_DATA_TOPICS = [
-    str(topics.DAS.data),
-    str(topics.PowerModel.recommended_sp),
-    str(topics.PowerModel.predicted_max_speed),
-    str(topics.PowerModel.plan_name),
-]
 
-V3_MESSAGE = [
-    str(topics.DAShboard.receive_message)
-]
-
-class Data:
+class Data(ABC):
     """ A class to keep track of the most recent bike data for the overlays.
 
         Data comes into the class in the V2/V3 MQTT formats and may be accessed
-        by using this class as a dictionary. """
+        by using this class as a dictionary. This class is implemented by
+        versions for specific bikes (DataV2, DataV3,...) """
 
     data_types = {
         # DAS data
@@ -60,31 +52,6 @@ class Data:
         self.message_received_time = 0
         self.message_duration = 5 # seconds
 
-    def load_v2_query_string(self, data: str) -> None:
-        """ Updates stored fields with data stored in a V2 query string,
-            e.g. `power=200&cadence=95`.
-
-            Only the supplied data fields will be updated, the rest remain as
-            they were. """
-        terms = data.split("&")
-        for term in terms:
-            key, value = term.split("=")
-            if key not in self.data_types:
-                continue
-            cast_func = self.data_types[key]
-            self.data[key] = cast_func(value)
-
-    def load_v3_module_data(self, data: str) -> None:
-        """ Updates stored fields with data from a V3 sensor module. """
-        # TODO
-        raise NotImplementedError
-
-    def load_v3_message(self, data: str) -> None:
-        """ Accepts a V3 message packet which is made accessable via
-            self.get_message. """
-        self.message_received_time = time.time()
-        self.message = data
-
     def has_message(self) -> bool:
         """ Returns true if a message is available for display on the overlay,
             otherwise false.
@@ -116,3 +83,61 @@ class Data:
         else:
             print(f"WARNING: invalid data field `{field}` used")
             return None
+
+    @abstractmethod
+    def load_data(self, data: str) -> None:
+        """ Updates stored fields with data stored in an MQTT data packet.
+
+            Only the supplied data fields should be updated, the rest remain as
+            they were. This should be implemented by all Data subclasses """
+        pass
+
+    @abstractmethod
+    def load_message(self, data: str) -> None:
+        """ Stores a message which is made available by self.get_message.
+        
+            Should be implemented by all Data subclasses. """
+        pass
+
+
+class DataV2(Data):
+
+    data_topics = [
+        str(topics.DAS.data),
+        str(topics.PowerModel.recommended_sp),
+        str(topics.PowerModel.predicted_max_speed),
+        str(topics.PowerModel.plan_name),
+    ]
+    message_topic = str(topics.DAShboard.receive_message)
+
+    def load_data(self, data: str) -> None:
+        """ Updates stored fields with data stored in a V2 query string,
+            e.g. `power=200&cadence=95`. """
+        terms = data.split("&")
+        for term in terms:
+            key, value = term.split("=")
+            if key not in self.data_types:
+                continue
+            cast_func = self.data_types[key]
+            self.data[key] = cast_func(value)
+
+    def load_message(self, data: str) -> None:
+        """ Stores a V3 message packet. """
+        self.message_received_time = time.time()
+        self.message = data
+
+
+class DataV3(Data):
+
+    data_topics = []
+    message_topic = str(topics.DAShboard.receive_message)
+
+    def load_data(self, data: str) -> None:
+        """ Updates stored fields with data from a V3 sensor module data
+            packet. """
+        pass
+
+    def load_message(self, data: str) -> None:
+        """ Stores a V3 message packet. """
+        self.message_received_time = time.time()
+        self.message = data
