d686780e209e374f0141d4b3276b7dbd34de17df
Christopher <hallgchris@gmail.com>
2020-07-13 12:01:16 +1000

    Document component methods

--- components/component.py
+++ components/component.py
@@ -4,9 +4,13 @@ from canvas import Canvas
 from data import Data
 
 class Component(ABC):
+
     @abstractmethod
     def draw_base(self, canvas: Canvas):
-        pass
+        """ Called a single time in order to draw the static, unchanging parts
+            of this overlay component. """
+
     @abstractmethod
     def draw_data(self, canvas: Canvas, data: Data):
-        pass
+        """ Called at a regular interval to update dynamic data being displayed
+            on the overlay from this component. """
