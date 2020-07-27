a61dea738369c82ac870727ae825e935fadebc98
nix-chu <nmchua8@gmail.com>
2020-07-27 22:09:28 +1000

    Append /v3 prefix for camera errors topic

--- topics.py
+++ topics.py
@@ -37,7 +37,7 @@ class Camera(Topic):
     get_overlays = 'camera/get_overlays'
     set_overlay = 'camera/set_overlay'
     push_overlays = 'camera/push_overlays'
-    errors = 'camera/errors'
+    errors = '/v3/camera/errors'
 
 class DAShboard(Topic):
     """DAShboard MQTT Topics"""
