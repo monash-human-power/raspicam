6ea476759c7af3b7e4fff16ded5c146eae9ec2ad
nix-chu <nmchua8@gmail.com>
2020-09-18 00:40:00 +1000

    Assign boolean to client when connected

--- overlay.py
+++ overlay.py
@@ -50,6 +50,7 @@ class Overlay(ABC):
         self.client.on_connect = self._on_connect
         self.client.on_disconnect = self.on_disconnect
         self.client.on_log = self.on_log
+        self.client.is_connected = False # Default is false until connected
 
         # Set the video feed status to offline if connection breaks
         video_topic = f"{str(DAShboard.status_video_feed)}/{self.device}"
@@ -170,6 +171,7 @@ class Overlay(ABC):
         self.client.subscribe(str(DAShboard.recording))
         with self.exception_handler:
             self.on_connect(client, userdata, flags, rc)
+        self.client.is_connected = True
 
     def on_data_message(self, client, userdata, msg):
         with self.exception_handler:
