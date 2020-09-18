6000287c3f2a124d18f44a567e551bc00c6a504c
nix-chu <nmchua8@gmail.com>
2020-09-18 22:05:17 +1000

    Create DAShboardMessage class to support sending messages from DAShboard

---
+++ components/dashboard_message.py
@@ -0,0 +1,16 @@
+from components.message import Message
+from canvas import Canvas, Colour
+
+
+class DAShboardMessage(Message):
+    """Displays messages sent from the DAShboard onto the camera overlay.
+
+    Messages are only sent when the overlay is connected to the DAS.
+    """
+
+    def draw_data(self, canvas: Canvas, data: Data) -> None:
+        if not data.has_message():
+            return
+        message = data.get_message()
+        display_str = f"Message: {message}"
+        self.display_message(canvas, display_str)
