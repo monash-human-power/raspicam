86d6ac627c985f49b39f33c2c4cdb1c2d4135bf9
nix-chu <nmchua8@gmail.com>
2020-09-18 22:08:15 +1000

    Fix bug where draw_data method was not accepting data parameter

--- components/das_disconnect_message.py
+++ components/das_disconnect_message.py
@@ -1,29 +1,26 @@
 from components.message import Message
 from canvas import Canvas, Colour
+from data import Data
 
 
 class DASDisconnectMessage(Message):
     """Class displays error message on overlay when DAS is not
     connected, or overlay is not connected to an MQTT broker.
 
-    Extends the Message class.
-
     Attributes:
         text_colour: Colour type representing the colour of the message
     """
 
     text_colour = Colour.red
+    message = "Message: DAS is disconnected. Connect to a broker."
 
-    def draw_data(self, canvas: Canvas):
+    def draw_data(self, canvas: Canvas, data: Data):
         """Set up the disconnected message and pass it into the
         display_message method.
 
         Display message only whenever DAS is disconnected or overlay is not
         connected to broker.
         """
-        disconnected_message = (
-            "Message: DAS is disconnected. Connect to a broker."
-        )
         self.display_message(
-            canvas, disconnected_message, DASDisconnectMessage.text_colour
+            canvas, DASDisconnectMessage.message, DASDisconnectMessage.text_colour
         )
