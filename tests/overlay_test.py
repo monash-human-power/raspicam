cefce3fc0967e659f2eea6e0f22146a7292bf69a
Christopher Hall <hallgchris@gmail.com>
2020-09-21 22:51:53 +1000

    Add tests to check that overlays can be instantiated (#49)
    * Add tests to check that overlays can be instantiated
    
    * Remove extra newline

---
+++ tests/overlay_test.py
@@ -0,0 +1,24 @@
+from overlay_all_stats import OverlayAllStats
+from overlay_blank import OverlayBlank
+from overlay_new import OverlayNew
+from overlay_top_strip import OverlayTopStrip
+
+
+def test_overlay_all_stats_instantiation():
+    """ Check that OverlayAllStats can be instantiated """
+    OverlayAllStats("V3")
+
+
+def test_overlay_blank_instantiation():
+    """ Check that OverlayBlank can be instantiated """
+    OverlayBlank("V3")
+
+
+def test_overlay_new_instantiation():
+    """ Check that OverlayNew can be instantiated """
+    OverlayNew("V3")
+
+
+def test_overlay_top_strip_instantiation():
+    """ Check that OverlayTopStrip can be instantiated """
+    OverlayTopStrip("V3")
