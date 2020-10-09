18e7af90964240a67238f2241f2d6b514960c696
nix-chu <nmchua8@gmail.com>
2020-10-09 14:04:43 +1100

    Rename component to Transparent

--- components/translucent.py
+++ components/translucent.py
@@ -2,8 +2,8 @@ from components import Component
 from canvas import Canvas, Colour
 from data import Data
 
-class Translucent(Component):
-    """A translucent rectangle behind the data overlay for data values to stand out
+class Transparent(Component):
+    """A transparent rectangle behind the data overlay for data values to stand out
     from a white background (white racetrack, lines, etc).
     """
     TOP_LEFT_COORDS = (0, 570)
@@ -12,9 +12,9 @@ class Translucent(Component):
 
     def draw_base(self, canvas: Canvas):
         canvas.draw_rect(
-            Translucent.TOP_LEFT_COORDS,
-            Translucent.BOTTOM_RIGHT_COORDS,
-            Translucent.COLOUR
+            Transparent.TOP_LEFT_COORDS,
+            Transparent.BOTTOM_RIGHT_COORDS,
+            Transparent.COLOUR
         )
 
     def draw_data(self, canvas: Canvas, data: Data):
