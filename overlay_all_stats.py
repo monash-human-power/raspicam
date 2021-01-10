fa5e56fd95ea2e224ae827110e4e936642bb64ae
nix-chu <nmchua8@gmail.com>
2021-01-10 23:26:25 +1100

    Append DataValue.get() methods to accessing data values

--- overlay_all_stats.py
+++ overlay_all_stats.py
@@ -32,51 +32,57 @@ class OverlayAllStats(Overlay):
         self.data_canvas.clear()
         self.message_canvas.clear()
 
-        if self.data["power"] != 0:
+        if self.data["power"].get() != 0:
             self.draw_power_rec_power()
 
-        if self.data["cadence"] != 0:
+        if self.data["cadence"].get() != 0:
             self.draw_cadence()
 
-        if self.data["reed_velocity"] != 0:
+        if self.data["reed_velocity"].get() != 0:
             self.draw_max_rec_reed_velocity()
 
-        if self.data["reed_distance"] != 0:
+        if self.data["reed_distance"].get() != 0:
             self.draw_distance()
 
         if self.data.has_message():
             self.draw_messages()
 
     def draw_power_rec_power(self):
-        power = self.data["power"]
-        rec_power = self.data["rec_power"]
+        power = self.data["power"].get_string()
+        rec_power = self.data["rec_power"].get_string()
         tolerance = 0.05
+
         # Display recommended power
+        rec_power_text = "{0}".format(round(rec_power, 2))
         self.data_canvas.draw_text(
-            "{0}".format(round(rec_power, 2)), (340, self.text_height * 1)
+            rec_power_text, (340, self.text_height * 1)
         )
+
         # Display power
+        power_text = "{0}".format(round(power, 2))
         if power > (rec_power + (rec_power * tolerance)):
             power_colour = Colour.red
         elif power > rec_power:
             power_colour = Colour.green
         else:
             power_colour = Colour.black
+
         self.data_canvas.draw_text(
-            "{0}".format(round(power, 2)),
+            power_text,
             (340, self.text_height * 2),
             colour=power_colour,
         )
 
     def draw_cadence(self):
-        cadence = self.data["cadence"]
+        cadence = self.data["cadence"].get_string()
+        cadence_text = "{0}".format(round(cadence, 2))
         self.data_canvas.draw_text(
-            "{0}".format(round(cadence, 2)), (340, self.text_height * 3)
+            cadence_text, (340, self.text_height * 3)
         )
 
     def draw_max_rec_reed_velocity(self):
         # Max Speed
-        max_speed = self.data["predicted_max_speed"]
+        max_speed = self.data["predicted_max_speed"].get_string()
         max_speed_text = "{0} km/h".format(round(max_speed, 2))
         max_speed_pos = (
             self.width // 2 - 70,
@@ -85,7 +91,7 @@ class OverlayAllStats(Overlay):
         self.data_canvas.draw_text(max_speed_text, max_speed_pos, size=2.5)
 
         # Recommended speed
-        rec_speed = self.data["rec_speed"]
+        rec_speed = self.data["rec_speed"].get_string()
         rec_speed_text = "{0} km/h".format(round(rec_speed, 2))
         rec_speed_pos = (
             self.width // 2 - 70,
@@ -94,7 +100,7 @@ class OverlayAllStats(Overlay):
         self.data_canvas.draw_text(rec_speed_text, rec_speed_pos, size=2.5)
 
         # Actual speed
-        speed = self.data["reed_velocity"]
+        speed = self.data["reed_velocity"].get_string()
         speed_text = "{0} km/h".format(round(speed, 2))
         speed_pos = (self.width // 2 - 70, self.height - self.speed_height * 0)
         tolerance = 0.05
@@ -109,9 +115,10 @@ class OverlayAllStats(Overlay):
         )
 
     def draw_distance(self):
-        reed_distance = self.data["reed_distance"]
+        reed_distance = self.data["reed_distance"].get_string()
+        reed_distance_text = "{0}".format(round(reed_distance, 2))
         self.data_canvas.draw_text(
-            "{0}".format(round(reed_distance, 2)), (340, self.text_height * 4)
+            reed_distance_text, (340, self.text_height * 4)
         )
 
     def draw_messages(self):
