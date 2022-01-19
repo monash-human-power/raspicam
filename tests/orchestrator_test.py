2e90c2792521ed18a12ab96b00a031de7a82f215
Eric <eyem0002@student.monsah.edu>
2022-01-19 20:50:05 +1100

    fixed unit testing to account for username parsing

--- tests/orchestrator_test.py
+++ tests/orchestrator_test.py
@@ -4,11 +4,11 @@ import orchestrator
 
 
 class TestOrchestrator:
-    """ Orchestrator test class """
+    """Orchestrator test class"""
 
     @staticmethod
     def test_class_instantiation():
-        """ Test out class instantiation """
+        """Test out class instantiation"""
         broker_ip = "localhost"
         port = 1883
         test_orchestrator = orchestrator.Orchestrator(broker_ip, port)
@@ -26,27 +26,36 @@ class TestOrchestrator:
 
 
 class TestGetArgs:
-    """ get_args test class """
+    """get_args test class"""
 
     @staticmethod
     def test_valid_arguments():
-        """ get_args valid arguments
+        """get_args valid arguments
         Test the arguments parser for the file
         """
         test_input = vars(orchestrator.get_args())
-        expected_result = {"host": read_configs()["broker_ip"]}
+        expected_result = {
+            "host": read_configs()["broker_ip"],
+            "username": None,
+        }
         assert (
             test_input == expected_result
         ), "failed - default ip in function get_args is incorrect"
 
         test_input = ["--host", "localhost"]
-        expected_result = {"host": "localhost"}
+        expected_result = {"host": "localhost", "username": None}
         assert (
             vars(orchestrator.get_args(test_input)) == expected_result
         ), "failed - ip of local host is incorrect"
 
         test_input = ["--host", "192.168.100.100"]
-        expected_result = {"host": "192.168.100.100"}
+        expected_result = {"host": "192.168.100.100", "username": None}
         assert (
             vars(orchestrator.get_args(test_input)) == expected_result
         ), "failed - Broker ip not same as default"
+
+        test_input = ["--host", "192.168.100.100", "--username", "username"]
+        expected_result = {"host": "192.168.100.100", "username": "username"}
+        assert (
+            vars(orchestrator.get_args(test_input)) == expected_result
+        ), "failed - username not same as default"
