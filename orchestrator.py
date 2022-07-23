8e93731c9e1f6902be6c40b353c81869f0a44b71
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2022-07-23 22:13:49 +1000

    Remove updating of logging state, added detailed comment on logging.

--- orchestrator.py
+++ orchestrator.py
@@ -105,11 +105,14 @@ class Orchestrator:
         next_logging_state = not self.currently_logging
         msg = dumps({"start": next_logging_state})
         self.mqtt_client.publish(topics.V3.start, msg)
-        self.set_logging_state(next_logging_state)
         print(f"Set logging state to {next_logging_state}")
 
         # `self.currently_logging` will be updated when we receive the message
         # we publish above.
+        # If start message is sent successfully, as we're subscribed to the topic
+        # `self.logging_state` will be updated in `on_message`
+        # If the message fails to send, we don't update internal logging state
+        # so the next button press causes it to try again
 
     def set_logging_state(self, logging: bool) -> None:
         """Set the data logging state of the camera, updating the LED."""
