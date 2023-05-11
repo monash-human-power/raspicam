c4534b81a846b1e03b50149fa62760205d8fada0
Cheuk Lam Winnie Chui <cchu0061@student.monash.edu>
2023-05-11 13:49:21 +1000

    check avg direction

--- data.py
+++ data.py
@@ -264,8 +264,8 @@ class DataV3(Data):
                 print("windSpeed found")
                 self.data["wind_speed"].update(sensor_value)
             elif sensor_name == "windDirection":
-                # self.data["wind_direction"].update(sensor_value)
-                self.data["wind_direction"] = 10
+                avgDirection = sensor_value["avgDirection"]
+                self.data["wind_direction"].update(avgDirection)
             elif sensor_name in self.data.keys():
                 self.data[sensor_name].update(sensor_value)
 
